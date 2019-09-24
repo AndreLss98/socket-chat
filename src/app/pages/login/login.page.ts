d05500e63e8fda1bc31735b7ab5e9f4b6032c6b0
Andre Luis <aisitiger.98@gmail.com>
2019-09-24 11:10:55 -0300

    chamando funcao para navegar para tela de home

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MainNavigateService } from 'src/app/services/navigator/main-navigate.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private navigateService: MainNavigateService) {
+
+  }
 
   ngOnInit() {
+
+  }
+
+  public toHome() {
+    this.navigateService.goToHome();
   }
 
 }
