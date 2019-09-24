8034c449a7eba6edcfbfbd77845e99cab8ffaa01
Andre Luis <aisitiger.98@gmail.com>
2019-09-24 12:01:57 -0300

    estruturando componente de conversa

--- src/app/components/conversation-item/conversation-item.component.ts
+++ src/app/components/conversation-item/conversation-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-conversation-item',
@@ -7,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConversationItemComponent implements OnInit {
 
-  constructor() { }
+  @Input() name: string;
+  @Input() lastMessage: string;
+  @Input() date;
 
-  ngOnInit() {}
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+
 
 }
