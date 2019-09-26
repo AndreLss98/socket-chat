15cf7ce06a609e5852aca933faa79c2a3baa0ae2
Andre Luis <aistiger.98@gmail.com>
2019-09-26 19:24:02 -0300

    implementando modulo da lib de socket no projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { environment } from './../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SocketIoModule.forRoot(environment.socketIoConifg)],
   providers: [
     StatusBar,
     SplashScreen,
