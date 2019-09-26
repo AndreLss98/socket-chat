429d8c345d8ab38d1b5953e1a1d5c1e50c4880d6
Andre Luis <aistiger.98@gmail.com>
2019-09-26 19:58:13 -0300

    criando listener de entrada de usuarios no socket

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MainNavigateService } from 'src/app/services/navigator/main-navigate.service';
 
+import { Socket } from 'ngx-socket-io';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,12 +10,14 @@ import { MainNavigateService } from 'src/app/services/navigator/main-navigate.se
 })
 export class HomePage implements OnInit {
 
-  constructor( private navigateService: MainNavigateService) {
+  constructor( private navigateService: MainNavigateService, private socket: Socket) {
 
   }
 
   ngOnInit() {
-    
+    this.socket.fromEvent('users-changed').subscribe(data => {
+      console.log('Got data: ', data);
+    });
   }
 
   public visualizeChat() {
