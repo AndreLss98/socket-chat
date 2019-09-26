c08ddd0f1550258c43d088e4a624e6429355b2d1
Andre Luis <aistiger.98@gmail.com>
2019-09-26 12:20:32 -0300

    validando campo de messagem de texto

--- src/app/pages/chat/chat.page.ts
+++ src/app/pages/chat/chat.page.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { ChatObject } from 'src/app/models/chat.model';
 
 import { ChatService } from 'src/app/services/chat/chat.service';
 import { GlobalConfgService } from 'src/app/services/settings/global-confg.service';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-chat',
@@ -11,13 +12,14 @@ import { GlobalConfgService } from 'src/app/services/settings/global-confg.servi
   styleUrls: ['./chat.page.scss'],
 })
 export class ChatPage implements OnInit {
-
+  
+  @ViewChild(IonContent, { static: false }) content: IonContent;
   public message = '';
   public messages: ChatObject[] = [];
   public currentUser;
 
   constructor(private chatService: ChatService, private settingsService: GlobalConfgService) {
-
+  
   }
 
   ngOnInit() {
@@ -37,6 +39,13 @@ export class ChatPage implements OnInit {
     }
     this.chatService.sendMessages(tempMessage);
     this.message = '';
+    this.scrollContent();
+  }
+
+  private scrollContent() {
+    setTimeout(() => {
+      this.content.scrollToBottom(200);
+    })
   }
 
 }
