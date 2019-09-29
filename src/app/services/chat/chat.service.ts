fd5df16e40910443c11bdee2b4d51ddb4b158319
Andre Luis <aistiger.98@gmail.com>
2019-09-29 14:38:03 -0300

    criando listerner de messagens

--- src/app/services/chat/chat.service.ts
+++ src/app/services/chat/chat.service.ts
@@ -2,16 +2,34 @@ import { Injectable } from '@angular/core';
 
 import { Socket } from 'ngx-socket-io';
 import { GlobalConfgService } from '../settings/global-confg.service';
+import { Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-  public users = [];
+  public users: any = [];
+  private chatUser: any;
+
+  public messages: any[] = [];
+
+  private listerMessages: Subscription;
 
   constructor(private socket: Socket, private settings: GlobalConfgService) {
 
+  }
+
+  public setChatUser(pos) {
+    this.chatUser = this.users[pos];
+  }
+
+  public getChatUser() {
+    return this.chatUser;
+  }
+
+  public clearMessages() {
+    this.messages = [];
   }
 
   public startConnection() {
@@ -25,9 +43,28 @@ export class ChatService {
 
   public createListenerUserRegistration() {
     this.socket.fromEvent('users-changed').subscribe(data => {
-      if (data['event'] === 'joined' && data['user'] !== this.settings.getUserName()) {
-        this.users.unshift({userName: data['user']});
+      this.users = data;
+      const pos = this.users.findIndex(element => element.userName === this.settings.getUserName());
+      if (pos > -1) {
+        this.users.splice(pos, 1);
       }
     });
+  }
+
+  public createListernerUserMessages() {
+    this.listerMessages = this.socket.fromEvent('send-private-message').subscribe(data => {
+      this.messages.push(data);
+    });
+  }
+
+  public stopListernerUserMessages() {
+    this.listerMessages.unsubscribe();
+  }
+
+  public sendMessageForUser(messageData) {
+    this.socket.fromEvent('send-message').subscribe(data => {
+      this.messages.push(data);
+    });
+    this.socket.emit('private-message', messageData);
   }
 }
